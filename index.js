faff45074a091be3a82061c8c6ce13f7a50c4aa8
Junior <teixeirajunior1710@gmail.com>
2022-05-10 22:03:45 -0300

    criação de migrations e credenciais DB

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require ('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const app = express();
+// const database = require('./database/index');
+const dotenv = require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Endpoint is working');
