011482eade3d2409bafd2200a3e9b6dd84872d6f
Junior <teixeirajunior1710@gmail.com>
2022-05-18 20:35:09 -0300

    fix foreign key bug on delete post endpoint

--- database/migration/1652398270883_create_table_posts.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    "up": "CREATE TABLE db.posts (id INT auto_increment NOT NULL, CONSTRAINT posts_PK PRIMARY KEY (id), title VARCHAR(100) NULL, content LONGTEXT NULL, author_id INT NOT NULL, CONSTRAINT posts_FK FOREIGN KEY (author_id) REFERENCES db.posts(id), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci",
-    "down": ""
-}
--- database/migration/1652399622301_create_table_comments.js
+++ database/migration/1652399622301_create_table_comments.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE db.comments (comment_id INT auto_increment NOT NULL, content MEDIUMTEXT NOT NULL, comment_author TEXT NULL, post_id INT NOT NULL, CONSTRAINT comments_PK PRIMARY KEY (comment_id), CONSTRAINT comments_FK FOREIGN KEY (comment_id) REFERENCES db.posts(id) ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci",
+    "up": "CREATE TABLE db.comments (comment_id INT auto_increment NOT NULL, content MEDIUMTEXT NOT NULL, comment_author TEXT NULL, post_id INT NOT NULL, CONSTRAINT comments_PK PRIMARY KEY (comment_id), CONSTRAINT comments_FK FOREIGN KEY (comment_id) REFERENCES db.posts(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci",
     "down": ""
 }
